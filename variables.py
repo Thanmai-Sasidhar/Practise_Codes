d67fcb102a103483b18aea7c2e77bd6fba3bf786
Avanigadda Thanmai Sasidhar <atsasidhar@gmail.com>
2026-02-13 17:26:32 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,50 @@
+Python 3.13.0 (v3.13.0:60403a5409f, Oct  7 2024, 00:37:40) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+#variable
+a=2
+print(a)
+2
+s="Hello"
+print(s)
+Hello
+>>> y=True
+>>> print(y)
+True
+>>> b=100
+>>> print(b)
+100
+>>> del b
+>>> #Variable Namings
+>>> a0123="123"
+>>> print(a0123)
+123
+>>> #Special Characters are not allowed except "_" in the variables
+>>> #We cannot start the variales with numbers
+>>> city="Vijayawada"
+>>> print(city)
+Vijayawada
+>>> #We cannot use keywords as variables
+>>> colour="Blue"
+>>> print("colour")
+colour
+>>> #We should not use space in the variables instead we can use "_"
+>>> #first name --> its leads to error
+>>> first_name="Sasidhar"
+>>> print(first_name)
+Sasidhar
+>>> #we can use the _ at the starting of the variable
+>>> _a=10
+>>> print(_a)
+10
+>>> #For initializing the values in a single line we have to use ";" between the variables
+>>> a=6;b=7
+>>> print(a,b)
+6 7
+>>> fname,lname="Thanmai","Avanigadda"
+>>> print(fname,lname)
+Thanmai Avanigadda
+>>> print(fname+" "+lname)
+Thanmai Avanigadda
+>>> print(fname+lname)
+ThanmaiAvanigadda
+>>> #We can delete the varibales using "del" keyword
