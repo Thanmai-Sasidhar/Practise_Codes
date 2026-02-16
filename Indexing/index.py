a2a7bee5fa48adc8a786a9f3cad49f4d1559148c
Avanigadda Thanmai Sasidhar <atsasidhar@gmail.com>
2026-02-16 22:04:40 +0530

    Day 5

---
+++ Indexing/index.py
@@ -0,0 +1,72 @@
+Python 3.13.5 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a = "data science"
+
+a[0]+a[1]+a[2]+a[3]
+'data'
+
+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]
+'science'
+
+a[4]
+' '
+
+a[0]+a[1]
+'da'
+
+a[2]+a[3]
+'ta'
+
+
+a = "hello world"
+
+a[0]+a[1]+a[2]+a[3]+a[4]
+'hello'
+
+a[6]+a[7]+a[8]+a[9]+a[10]
+'world'
+
+a[5]
+' '
+
+a[0]+a[1]+a[2]
+'hel'
+
+a[8]+a[9]+a[10]
+'rld'
+
+
+a = "machine learning"
+
+a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]
+'machine'
+
+a[8]+a[9]+a[10]+a[11]+a[12]+a[13]+a[14]+a[15]
+'learning'
+
+a[7]
+' '
+
+a[0]+a[1]+a[2]
+'mac'
+
+a[10]+a[11]+a[12]
+'rni'
+
+
+a = "good morning"
+
+a[0]+a[1]+a[2]+a[3]
+'good'
+
+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]
+'morning'
+
+a[4]
+' '
+
+a[1]+a[2]
+'oo'
+
+a[8]+a[9]
+'ni'
