ce4068d9bd0317aa04b1cf8a622b480babad709e
Avanigadda Thanmai Sasidhar <atsasidhar@gmail.com>
2026-03-31 22:57:48 +0530

    Day 31

---
+++ Module,Packages&Libraries/Modules/module_1.py
@@ -0,0 +1,34 @@
+#module_1.py                                        
+
+'''
+def greetings(name):
+    print("Hello",name)
+'''
+
+'''
+a=3;b=4
+print("The sum is",a+b)
+'''
+                
+'''            
+Details ={"idnos":[10,20,30],
+        "names":["Thanmai","Sasidhar","Avanigadda"],        
+        "city":["Vij","Hyd","Viz"]}         
+'''
+
+'''
+if __name__=="__main__":
+    a=[10,20,30,40,50]              
+    a.append("code")
+    a.extend("code")
+    print(a)
+'''
+
+                                    
+def dummy():
+    if __name__=="__main__":
+        print("The program runs on script")
+    else:
+        print("The program runs in module")
+dummy()         
+            
---
+++ Module,Packages&Libraries/Modules/module_2.py
@@ -0,0 +1,27 @@
+#module_2.py                        
+
+import module_1 as m
+
+'''            
+m.greetings("Sasidhar")       
+'''
+
+'''
+a=m.Details["idnos"]
+b=m.Details["names      "]
+print(a,b)              
+'''
+'''
+
+#No need to write below program just given for understanding purpose just need to call dummy() 
+
+def dummy():
+    if __name__=="__main__":
+        print("The program runs on script") #As this shows while running it directly in this python file
+    else:
+        print("The program runs in module") # It shows when the module_1.py is called
+'''         
+m.dummy()                            
+
+    
+            
