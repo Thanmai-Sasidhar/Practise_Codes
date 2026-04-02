2e4fbfe06bcf0a9bb5f18372daf51f5e8d1ad540
Avanigadda Thanmai Sasidhar <atsasidhar@gmail.com>
2026-04-02 22:55:14 +0530

    Day 33
    Implement date and time module to display current date and time.

---
+++ Module,Packages&Libraries/date_and_time_module.py
@@ -0,0 +1,27 @@
+
+#date and time module
+'''
+from datetime import date
+
+a=date.today()
+print(a)
+
+import datetime
+b=datetime.datetime.now()
+print(b)
+'''
+
+import time
+'''
+a=time.time()
+print(a)
+
+b=time.localtime(a)
+print(b)
+
+print(f"Today date is {b.tm_mday}-{b.tm_mon}-{b.tm_year}")
+
+print(f"Today time is {b.tm_hour}:{b.tm_min}:{b.tm_sec}")
+
+print(f"{b.tm_wday}-{b.tm_mday}-{b.tm_yday}") #Not clear one 
+'''
